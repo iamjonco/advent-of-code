88072282abe4576cca83673278f26a7f2ec6f4dc
Jon Co <jonathanco1991@gmail.com>
2020-12-15 07:00:10 +0000

    perf(2020): improved day 15 solution

--- aoc/2020/day15.py
+++ aoc/2020/day15.py
@@ -2,24 +2,12 @@ def get_nth(numbers, nth):
     if nth - 1 < len(numbers):
         return numbers[nth - 1]
 
-    turn = len(numbers) + 1
     prev = numbers[-1]
-    tracker = {n: [i + 1] for i, n in enumerate(numbers)}
-
-    for i in range(turn, nth + 1):
-        spoken = tracker.get(prev)
-        if len(spoken) < 2:
-            prev = 0
-        else:
-            prev = spoken[-1] - spoken[-2]
-
-        spoken = tracker.get(prev)
-        if spoken is None:
-            tracker[prev] = [turn]
-        else:
-            spoken.append(turn)
-
-        turn += 1
+    last_spoken = {n: i for i, n in enumerate(numbers)}
+    for t in range(len(numbers) - 1, nth - 1):
+        next = t - last_spoken.get(prev, t)
+        last_spoken[prev] = t
+        prev = next
 
     return prev
 
