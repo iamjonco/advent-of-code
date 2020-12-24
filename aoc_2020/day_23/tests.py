0b3892510da5fecab8478c8707bb230460ac1806
Jon Co <jonathanco1991@gmail.com>
2020-12-24 06:59:31 +0000

    refactor(2020): modify day 23 tests to use parameters

--- aoc_2020/day_23/tests.py
+++ aoc_2020/day_23/tests.py
@@ -1,11 +1,17 @@
+import pytest
+
 from aoc_2020.day_23 import part_1, part_2
 
 
-def test_part_1():
-    assert part_1("389125467") == 67384529
-    assert part_1("487912365") == 89573246
+@pytest.mark.parametrize(
+    "file,expected", [("389125467", 67384529), ("487912365", 89573246)]
+)
+def test_part_1(file, expected):
+    assert part_1(file) == expected
 
 
-def test_part_2():
-    assert part_2("389125467") == 149245887792
-    assert part_2("487912365") == 2029056128
+@pytest.mark.parametrize(
+    "file,expected", [("389125467", 149245887792), ("487912365", 2029056128)]
+)
+def test_part_2(file, expected):
+    assert part_2(file) == expected
