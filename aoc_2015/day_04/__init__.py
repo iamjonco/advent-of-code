09547c9e82bd9a03eacd11af62ade25546eb411e
Jon Co <jonathanco1991@gmail.com>
2021-01-07 12:46:33 +0000

    feat(2015): add day 4 solutions

--- aoc_2015/day_04/__init__.py
+++ aoc_2015/day_04/__init__.py
@@ -1,16 +1,23 @@
-import fileinput
+import hashlib
+
+secret_key = "bgvyzdsv"
 
 
-def get_inputs(filename=None):
-    return fileinput.input(filename or "inputs.txt")
+def calc_number(hash_start):
+    i = 0
+    md5_hash = ""
+    while not md5_hash.startswith(hash_start):
+        i += 1
+        md5_hash = hashlib.md5(f"{secret_key}{i}".encode("utf-8")).hexdigest()
+    return i
 
 
 def part_1(filename=None):
-    pass
+    return calc_number("00000")
 
 
 def part_2(filename=None):
-    pass
+    return calc_number("000000")
 
 
 if __name__ == "__main__":
