2b9f501a4970b40fa2900719e76f48311d22cb2d
Jon Co <jonathanco1991@gmail.com>
2021-01-07 12:39:01 +0000

    feat(2015): add day 3 solutions

--- aoc_2015/day_03/__init__.py
+++ aoc_2015/day_03/__init__.py
@@ -2,15 +2,48 @@ import fileinput
 
 
 def get_inputs(filename=None):
-    return fileinput.input(filename or "inputs.txt")
+    with fileinput.input(filename or "inputs.txt") as fi:
+        return next(fi).strip()
+
+
+def move(x: int, y: int, d: str):
+    if d == "^":
+        y += 1
+    elif d == "v":
+        y -= 1
+    elif d == ">":
+        x += 1
+    elif d == "<":
+        x -= 1
+    return x, y
 
 
 def part_1(filename=None):
-    pass
+    dirs = get_inputs(filename)
+    x = 0
+    y = 0
+    visited = {(x, y)}
+    for d in dirs:
+        x, y = move(x, y, d)
+        visited.add((x, y))
+    return len(visited)
 
 
 def part_2(filename=None):
-    pass
+    dirs = get_inputs(filename)
+    sx, sy = 0, 0
+    rx, ry = 0, 0
+    visited = {(0, 0)}
+    santa = True
+    for d in dirs:
+        if santa:
+            sx, sy = move(sx, sy, d)
+            visited.add((sx, sy))
+        else:
+            rx, ry = move(rx, ry, d)
+            visited.add((rx, ry))
+        santa = not santa
+    return len(visited)
 
 
 if __name__ == "__main__":
