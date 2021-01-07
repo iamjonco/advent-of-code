2108a4af05c0f662a24d0edc743094b990590f4b
Jon Co <jonathanco1991@gmail.com>
2021-01-07 12:23:48 +0000

    feat(2015): add day 2 solutions

--- aoc_2015/day_02/__init__.py
+++ aoc_2015/day_02/__init__.py
@@ -2,15 +2,29 @@ import fileinput
 
 
 def get_inputs(filename=None):
-    return fileinput.input(filename or "inputs.txt")
+    return (
+        [int(i) for i in x]
+        for x in (
+            l.strip().split("x") for l in fileinput.input(filename or "inputs.txt")
+        )
+    )
 
 
 def part_1(filename=None):
-    pass
+    presents = get_inputs(filename)
+    sides = map(lambda p: [p[0] * p[1], p[1] * p[2], p[2] * p[0]], presents)
+    return sum(min(s) + 2 * s[0] + 2 * s[1] + 2 * s[2] for s in sides)
 
 
 def part_2(filename=None):
-    pass
+    presents = get_inputs(filename)
+    return sum(
+        p[0] * p[1] * p[2]  # volume
+        + min(
+            [(2 * p[0] + 2 * p[1]), (2 * p[1] + 2 * p[2]), (2 * p[2] + 2 * p[0])]
+        )  # calc all distances
+        for p in presents
+    )
 
 
 if __name__ == "__main__":
