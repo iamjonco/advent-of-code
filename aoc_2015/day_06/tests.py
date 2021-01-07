01d56a41ce7c993ae02b9bd5841144e480cdf2a9
Jon Co <jonathanco1991@gmail.com>
2021-01-07 11:48:08 +0000

    feat(2015): add 2015 scaffold

---
+++ aoc_2015/day_06/tests.py
@@ -0,0 +1,9 @@
+from aoc_2020.day_06 import part_1, part_2
+
+
+def test_part_1():
+    assert part_1("inputs.txt") == 0
+
+
+def test_part_2():
+    assert part_2("inputs.txt") == 0
---
+++ aoc_2015/day_17/__init__.py
@@ -0,0 +1,19 @@
+import fileinput
+
+
+def get_inputs(filename=None):
+    return fileinput.input(filename or "inputs.txt")
+
+
+def part_1(filename=None):
+    pass
+
+
+def part_2(filename=None):
+    pass
+
+
+if __name__ == "__main__":
+    print("Day 17")
+    print(f"Part 1: {part_1()}")
+    print(f"Part 2: {part_2()}")
