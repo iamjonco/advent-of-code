8bda6448ebf05d79823d75b084926aa8f47fa6ec
Jon Co <jonathanco1991@gmail.com>
2021-01-08 13:35:16 +0000

    feat(2015): add day 5 solutions

--- aoc_2015/day_05/__init__.py
+++ aoc_2015/day_05/__init__.py
@@ -1,16 +1,30 @@
 import fileinput
+import re
 
 
 def get_inputs(filename=None):
-    return fileinput.input(filename or "inputs.txt")
+    return (l.strip() for l in fileinput.input(filename or "inputs.txt"))
 
 
 def part_1(filename=None):
-    pass
+    vowels = re.compile(r"([aeiou].*){3,}")
+    double = re.compile(r"(.)\1")
+    illegal = re.compile(r"ab|cd|pq|xy")
+    return sum(
+        1
+        for s in get_inputs(filename)
+        if vowels.search(s) and double.search(s) and not illegal.search(s)
+    )
 
 
 def part_2(filename=None):
-    pass
+    double = re.compile(
+        r"(..).*\1"
+    )  # match any two chars then find the same capture group anywhere after
+    sandwich = re.compile(r"(.).\1")  # match repeated char with one char in between
+    return sum(
+        1 for s in get_inputs(filename) if double.search(s) and sandwich.search(s)
+    )
 
 
 if __name__ == "__main__":
--- aoc_2015/day_05/tests.py
+++ aoc_2015/day_05/tests.py
@@ -2,8 +2,8 @@ from aoc_2015.day_05 import part_1, part_2
 
 
 def test_part_1():
-    assert part_1("inputs.txt") == 0
+    assert part_1("inputs.txt") == 258
 
 
 def test_part_2():
-    assert part_2("inputs.txt") == 0
+    assert part_2("inputs.txt") == 53
