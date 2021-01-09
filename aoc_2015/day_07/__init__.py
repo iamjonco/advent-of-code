19ae9a696d6d281a461dead388e62f9615579378
Jon Co <jonathanco1991@gmail.com>
2021-01-09 18:53:59 +0000

    feat(2015): add day 7 solutions

--- aoc_2015/day_07/__init__.py
+++ aoc_2015/day_07/__init__.py
@@ -1,16 +1,74 @@
 import fileinput
 
 
+class cached(object):
+    def __init__(self, arg, cache=None):
+        self._arg = arg
+        self.cache = cache or {}
+
+    def __call__(self, graph, k):
+        if k not in self.cache:
+            self.cache[k] = self._arg(graph, k)
+        return self.cache[k]
+
+
 def get_inputs(filename=None):
-    return fileinput.input(filename or "inputs.txt")
+    graph = {}
+    for l in fileinput.input(filename or "inputs.txt"):
+        parts = l.strip().split()
+        if len(parts) == 3:
+            graph[parts[-1]] = ("EQ", parts[0])
+        elif len(parts) == 4:
+            graph[parts[-1]] = (parts[0], parts[1])
+        else:
+            graph[parts[-1]] = (parts[1], parts[0], parts[2])
+    return graph
+
+
+def find_key(graph, k, cache):
+    if k in cache:
+        return cache[k]
+
+    if k.isdigit():
+        return int(k)
+
+    instruction = graph[k]
+    op = instruction[0]
+
+    if op == "AND":
+        value = find_key(graph, instruction[1], cache) & find_key(
+            graph, instruction[2], cache
+        )
+    elif op == "OR":
+        value = find_key(graph, instruction[1], cache) | find_key(
+            graph, instruction[2], cache
+        )
+    elif op == "LSHIFT":
+        value = find_key(graph, instruction[1], cache) << find_key(
+            graph, instruction[2], cache
+        )
+    elif op == "RSHIFT":
+        value = find_key(graph, instruction[1], cache) >> find_key(
+            graph, instruction[2], cache
+        )
+    elif op == "NOT":
+        value = ~find_key(graph, instruction[1], cache) & 0xFFFF
+    else:
+        value = find_key(graph, instruction[1], cache)
+
+    cache[k] = value
+    return value
 
 
 def part_1(filename=None):
-    pass
+    graph = get_inputs(filename)
+    return find_key(graph, "a", {})
 
 
 def part_2(filename=None):
-    pass
+    b = part_1(filename)
+    graph = get_inputs(filename)
+    return find_key(graph, "a", {"b": b})
 
 
 if __name__ == "__main__":
--- aoc_2015/day_07/tests.py
+++ aoc_2015/day_07/tests.py
@@ -2,8 +2,8 @@ from aoc_2015.day_07 import part_1, part_2
 
 
 def test_part_1():
-    assert part_1("inputs.txt") == 0
+    assert part_1("inputs.txt") == 3176
 
 
 def test_part_2():
-    assert part_2("inputs.txt") == 0
+    assert part_2("inputs.txt") == 14710
